3f09354d79c43f16e589f04ddf63780f03d24b60
Aysan Aghazadeh <aya34@pitt.edu>
2024-12-04 00:39:59 -0500

    Update LLAMA3_instruct.py

--- LLMs/LLAMA3_instruct.py
+++ LLMs/LLAMA3_instruct.py
@@ -16,10 +16,10 @@ class LLAMA3Instruct(nn.Module):
             )
             if args.fine_tuned:
                 self.model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
-                                                             token='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',
+                                                             token=args.token,
                                                              device_map='auto')
                 self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-instruct",
-                                                          token='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')
+                                                          token=args.token)
                 self.tokenizer.pad_token = self.tokenizer.eos_token
                 self.tokenizer.padding_side = "right"
                 self.model = PeftModel.from_pretrained(self.model,
@@ -35,7 +35,7 @@ class LLAMA3Instruct(nn.Module):
                 self.pipeline = pipeline(
                     "text-generation",
                     model=model_id,
-                    token='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',
+                    token=args.token,
                     model_kwargs={"torch_dtype": torch.bfloat16},
                     device_map="auto",
                 )
